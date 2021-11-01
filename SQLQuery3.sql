452d8558521286f15157bb759b677943e56a1d5c
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-11-01 20:02:15 +0100

    trigger all

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,39 +1,41 @@
 
-CREATE TRIGGER Ins_Ocena ON Ocena
+ALTER TRIGGER Ins_Ocena ON Ocena
 AFTER INSERT
 AS 
 BEGIN
 	INSERT INTO Ocena_Audit
-	(Akcija,Datum_Akcije,NewUcenik,NewPredmet,NewNastavnik,
+	(ID_Ocena, Akcija,Datum_Akcije,NewUcenik,NewPredmet,NewNastavnik,
 	NewOcena,NewDatum) 
-	(SELECT 'Dodavanje', GETDATE(),
+	(SELECT ID, 'Dodavanje', GETDATE(),
 	Ucenik, Predmet, Nastavnik, Ocena, Datum FROM inserted )
 END
-insert into ocena values('Aleksa', 'Baze', 'Pusic', 5, '2021-10-12')
+insert into ocena values('Neda', 'Baze', 'Pusic', 5, '2021-10-12')
 
-CREATE TRIGGER Del_Ocena ON Ocena
+ALTER TRIGGER Del_Ocena ON Ocena
 AFTER DELETE
 AS 
 BEGIN
 	INSERT INTO Ocena_Audit
-	(Akcija,Datum_Akcije,OldUcenik,OldPredmet,OldNastavnik,
+	(ID_Ocena, Akcija,Datum_Akcije,OldUcenik,OldPredmet,OldNastavnik,
 	OldOcena,OldDatum) 
-	(SELECT 'Brisanje', GETDATE(),
+	(SELECT ID, 'Brisanje', GETDATE(),
 	Ucenik, Predmet, Nastavnik, Ocena, Datum FROM deleted )
 END
 
 DELETE FROM Ocena WHERE ID=6
 
-CREATE TRIGGER Upd_Ocena ON Ocena
+ALTER TRIGGER Upd_Ocena ON Ocena
 AFTER UPDATE
 AS
 BEGIN
+IF UPDATE(ucenik) 
 INSERT INTO Ocena_Audit
-	SELECT 'Izmena', GETDATE(), Del.ucenik, Ins.ucenik,
+	SELECT Ins.ID, 'Izmena', GETDATE(), Del.ucenik, Ins.ucenik,
 	Del.predmet, Ins.predmet, Del.nastavnik, Ins.nastavnik, 
 	Del.ocena, Ins.ocena, Del.datum, Ins.datum 
 	FROM inserted AS Ins JOIN deleted AS Del ON Ins.ID = Del.ID
 END
 
-UPDATE Ocena 
-SET Ucenik='Pavle Georgijev', Ocena=6 WHERE ID=7
+UPDATE Ocena SET Ucenik='Pavle Georgijev', Ocena=6 WHERE ID=8
+
+SELECT * FROM Ocena_Audit WHERE ID_Ocena = 8
