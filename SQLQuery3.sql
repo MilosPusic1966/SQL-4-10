5b099977e16466f8739f336f6b30f0fc7f478afe
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-11-01 19:29:49 +0100

    trigger delete done

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -10,3 +10,17 @@ BEGIN
 	Ucenik, Predmet, Nastavnik, Ocena, Datum FROM inserted )
 END
 insert into ocena values('Aleksa', 'Baze', 'Pusic', 5, '2021-10-12')
+
+CREATE TRIGGER Del_Ocena ON Ocena
+AFTER DELETE
+AS 
+BEGIN
+	INSERT INTO Ocena_Audit
+	(Akcija,Datum_Akcije,OldUcenik,OldPredmet,OldNastavnik,
+	OldOcena,OldDatum) 
+	(SELECT 'Brisanje', GETDATE(),
+	Ucenik, Predmet, Nastavnik, Ocena, Datum FROM deleted )
+END
+
+DELETE FROM Ocena WHERE ID=6
+
